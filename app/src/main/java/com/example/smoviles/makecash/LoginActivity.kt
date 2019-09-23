4ff6611bdd7fdfb228bfe0e2a5473b02857318c4
AndresV97 <andres.villegasm@udea.edu.co>
2019-09-22 22:21:19 -0500

    commit Andrés

--- app/src/main/java/com/example/smoviles/makecash/LoginActivity.kt
+++ app/src/main/java/com/example/smoviles/makecash/LoginActivity.kt
@@ -3,7 +3,6 @@ package com.example.smoviles.makecash
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -16,6 +15,7 @@ import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -42,7 +42,7 @@ class LoginActivity : AppCompatActivity() {
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
         signOut = findViewById<View>(R.id.btn_login) as Button
         signOut.visibility = View.VISIBLE
-        signIn.setOnClickListener{view: View? -> signInGoogle ()
+        signIn.setOnClickListener{view: View? -> signInGoogle()
         }
 
         FirebaseAuth.getInstance().signOut()
@@ -56,7 +56,7 @@ class LoginActivity : AppCompatActivity() {
         updateUI(currentUser)
     }*/
 
-    private fun signInGoogle () {
+    private fun signInGoogle() {
         val signInIntent: Intent = mGoogleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
 
@@ -84,27 +84,48 @@ class LoginActivity : AppCompatActivity() {
     private fun handleResult (completedTask: Task<GoogleSignInAccount>) {
         try {
             val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java)
-            updateUI (account!!)
+            firebaseAuthWithGoogle(account!!)
+            Toast.makeText(this, "FirebaseAuthWithGoogle", Toast.LENGTH_SHORT).show()
         }catch (e: ApiException) {
-            Toast.makeText(this, "Bienvenido a Makecash", Toast.LENGTH_SHORT).show()
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            updateUI (null)
+            Toast.makeText(this, "UpdateUI  handle", Toast.LENGTH_SHORT).show()
         }
 
     }
 
-    private fun updateUI (account: GoogleSignInAccount) {
-        val dispTxt = findViewById<View>(R.id.tv_app) as TextView
-        tv_app.text = account.displayName
+    private fun updateUI (account: FirebaseUser?) {
+        if (account != null) {
+            var intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+            Toast.makeText(this, "Bienvenido a Makecash", Toast.LENGTH_SHORT).show()
+        }else if (account == null) {
+            Toast.makeText(this, "Falló el inicio de sesión", Toast.LENGTH_SHORT).show()
+        }
+        /*val dispTxt = findViewById<View>(R.id.tv_app) as TextView
+        tv_app.text = account?.displayName
         signOut.visibility = View.VISIBLE
         signOut.setOnClickListener {
             view: View? -> mGoogleSignInClient.signOut().addOnCompleteListener {
             task: Task<Void> -> dispTxt.text = " "
             signOut.visibility = View.INVISIBLE
             }
-        }
+        }*/
     }
 
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(acct?.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    updateUI(user)
+                }else {
+                    updateUI(null)
+                }
+
+            }
+    }
 }
 
 
